cf4834d46f6c5638c9d1f7d4226cccafe1d944a7
Bin Fan <binfan@cs.cmu.edu>
2014-08-25 23:10:56 -0700

    Cleanup test.cc

--- example/test.cc
+++ example/test.cc
@@ -1,21 +1,21 @@
 /* -*- Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+#include "cuckoofilter.h"
+
+#include <cassert>
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <cassert>
-
-#include "cuckoofilter.h"
 
 using namespace cuckoofilter;
 
 int main(int argc, char** argv) {
     const size_t bits_per_item = 12;
-    const size_t total_items   = 1000000;
+    const size_t total_items  = 1000000;
 
-    // simple one:
+    // Create a simple cuckoo filter
     CuckooFilter<size_t, bits_per_item> filter(total_items);
 
-    // insert items
+    // Insert items into this cuckoo filter
     size_t num_inserted = 0;
     for (size_t i = 0; i < total_items; i++, num_inserted++) {
         if (filter.Add(i) != Ok) {
@@ -23,15 +23,14 @@ int main(int argc, char** argv) {
         }
     }
 
-    // check existing items:
-    // every item should be there
+    // Check if previously inserted items are in the filter, expected
+    // true for all items
     for (size_t i = 0; i < num_inserted; i++) {
         assert(filter.Contain(i) == Ok);
     }
 
-    // checking non-existing items
-    // there are false positives
-    size_t total_queries = 0;   
+    // Check non-existing items, a few false positives expected
+    size_t total_queries = 0;
     size_t false_queries = 0;
     for (size_t i = total_items; i < 2 * total_items; i++) {
         if (filter.Contain(i) == Ok) {
@@ -40,7 +39,10 @@ int main(int argc, char** argv) {
         total_queries++;
     }
 
-    std::cout << "false positive rate is " << 100.0 * false_queries / total_queries << "%\n";
+    // Output the measured false positive rate
+    std::cout << "false positive rate is "
+              << 100.0 * false_queries / total_queries
+              << "%\n";
 
     return 0;
  }
