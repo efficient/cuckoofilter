93b7d8a8b3f30c5e1f49302d00eb03a93f12aac6
Bin Fan <binfan@cs.cmu.edu>
2014-09-08 23:54:19 -0700

    code leanup in example

--- example/test.cc
+++ example/test.cc
@@ -6,24 +6,24 @@
 #include <iostream>
 #include <vector>
 
-using namespace cuckoofilter;
+using cuckoofilter::CuckooFilter;
 
 int main(int argc, char** argv) {
     size_t total_items  = 1000000;
 
-    // Create a simple cuckoo filter, accepting keys of size_t and
-    // making 12 bits for each key:
+    // Create a cuckoo filter where each item is of type size_t and
+    // use 12 bits for each item:
     //    CuckooFilter<size_t, 12> filter(total_items);
-    // To use semi-sorting, define the cuckoo filter using
+    // To enable semi-sorting, define the storage of cuckoo filter to be
     // PackedTable, accepting keys of size_t type and making 13 bits
     // for each key:
     //   CuckooFilter<size_t, 13, PackedTable> filter(total_items);
     CuckooFilter<size_t, 12> filter(total_items);
 
-    // Insert items into this cuckoo filter
+    // Insert items to this cuckoo filter
     size_t num_inserted = 0;
     for (size_t i = 0; i < total_items; i++, num_inserted++) {
-        if (filter.Add(i) != Ok) {
+        if (filter.Add(i) != cuckoofilter::Ok) {
             break;
         }
     }
@@ -31,14 +31,14 @@ int main(int argc, char** argv) {
     // Check if previously inserted items are in the filter, expected
     // true for all items
     for (size_t i = 0; i < num_inserted; i++) {
-        assert(filter.Contain(i) == Ok);
+        assert(filter.Contain(i) == cuckoofilter::Ok);
     }
 
     // Check non-existing items, a few false positives expected
     size_t total_queries = 0;
     size_t false_queries = 0;
     for (size_t i = total_items; i < 2 * total_items; i++) {
-        if (filter.Contain(i) == Ok) {
+        if (filter.Contain(i) == cuckoofilter::Ok) {
             false_queries++;
         }
         total_queries++;
